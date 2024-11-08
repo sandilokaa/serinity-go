ba42dc9ddc763896d7c21e9a36956560958cec74
sandilokaa <sandyloka790@gmail.com>
2024-11-08 15:14:16 +0700

    docs: add midtrans

---
+++ payment/service.go
@@ -0,0 +1,56 @@
+package payment
+
+import (
+	"cheggstore/user"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	midtrans "github.com/veritrans/go-midtrans"
+)
+
+type service struct {
+}
+
+type Service interface {
+	GetPaymentURL(transaction Transaction, user user.User) (string, error)
+}
+
+func NewService() *service {
+	return &service{}
+}
+
+func (s *service) GetPaymentURL(transaction Transaction, user user.User) (string, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	midclient := midtrans.NewClient()
+	midclient.ServerKey = os.Getenv("SERVER_KEY")
+	midclient.ClientKey = os.Getenv("CLIENT_KEY")
+	midclient.APIEnvType = midtrans.Sandbox
+
+	snapGateway := midtrans.SnapGateway{
+		Client: midclient,
+	}
+
+	snapReq := &midtrans.SnapReq{
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID:  strconv.Itoa(transaction.ID),
+			GrossAmt: int64(transaction.Amount),
+		},
+		CustomerDetail: &midtrans.CustDetail{
+			Email: user.Email,
+			FName: user.Name,
+		},
+	}
+
+	snapTokenResp, err := snapGateway.GetToken(snapReq)
+	if err != nil {
+		return "", err
+	}
+
+	return snapTokenResp.RedirectURL, nil
+}
