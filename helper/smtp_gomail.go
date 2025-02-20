cc4a48ad7839d431f920c69523810df23f96bdc4
sandilokaa <sandyloka790@gmail.com>
2025-02-20 15:15:57 +0700

    docs: preparing smtp

---
+++ helper/smtp_gomail.go
@@ -0,0 +1,32 @@
+package helper
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/gomail.v2"
+)
+
+func SendEmail(to, subject, body string) error {
+	smtpHost := os.Getenv("SMTP_HOST")
+	smtpPort := 587
+	smtpEmail := os.Getenv("SMTP_EMAIL")
+	smtpPassword := os.Getenv("SMTP_PASSWORD")
+
+	mailer := gomail.NewMessage()
+	mailer.SetHeader("From", smtpEmail)
+	mailer.SetHeader("To", to)
+	mailer.SetHeader("Subject", subject)
+	mailer.SetBody("text/plain", body)
+
+	dialer := gomail.NewDialer(smtpHost, smtpPort, smtpEmail, smtpPassword)
+	err := dialer.DialAndSend(mailer)
+	if err != nil {
+		log.Printf("Failed to send email: %v", err)
+		return err
+	}
+
+	fmt.Printf("Email sent successfully")
+	return nil
+}
